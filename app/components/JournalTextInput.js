b5c807c667245cc78d8584d32b668312f2a86ff7
athomschke <astrid.thomschke@gmail.com>
2016-06-16 18:10:30 +0200

    Move redering of numbers in front of answers one level up #2

--- app/components/JournalTextInput.js
+++ app/components/JournalTextInput.js
@@ -1,6 +1,5 @@
 import React, { Component, PropTypes } from 'react';
 import {StyleSheet, TextInput, Text, View } from 'react-native';
-import { margin } from '../constants/style';
 
 export default class JournalTextInput extends Component {
 
@@ -34,46 +33,22 @@ export default class JournalTextInput extends Component {
     this.setState({text: text})
   }
 
-  renderDescription() {
-    if (this.props.description !== '') {
-      return (<Text style={[styles.description, this.props.styles.text]}>
-          {this.props.description}
-        </Text>
-      );
-    }
-  }
-
   render() {
     let that = this;
     return (
-      <View style={styles.container}>
-        {this.renderDescription()}
-        <View style={[styles.inputContainer, this.props.styles.input]}>
-          <TextInput style={[styles.input, this.props.styles.text]}
-            onChangeText={that.onChangeText.bind(that)}
-            value={that.state.text}
-          ></TextInput>
-        </View>
+      <View style={[styles.input, this.props.styles.input]}>
+        <TextInput style={this.props.styles.text}
+          onChangeText={that.onChangeText.bind(that)}
+          value={that.state.text}
+        ></TextInput>
       </View>
     )
   }
 }
+
 const styles = StyleSheet.create({
-  description: {
-    width: 15,
-    marginRight: margin
-  },
   input: {
     flex: 1,
     borderBottomWidth: 1,
-  },
-  inputContainer: {
-    flex: 1,
-    borderBottomWidth: 1
-  },
-  container: {
-    flexDirection: 'row',
-    margin: margin,
-    marginTop: 20
   }
 });
--- app/components/RoutineAnswer.js
+++ app/components/RoutineAnswer.js
@@ -1,32 +1,61 @@
 import React, { Component, PropTypes } from 'react';
+import {StyleSheet, Text, View } from 'react-native';
 import JournalTextInput from './JournalTextInput';
-import { View } from 'react-native';
+import { margin } from '../constants/style';
 
 export default class RoutineAnswer extends Component {
 
   static propTypes = {
     count: PropTypes.number,
-    numbered: PropTypes.bool
+    numbered: PropTypes.bool,
+    colors: PropTypes.object
   };
 
   static defaultProps = {
     count: 1,
-    numbered: false
+    numbered: false,
+    styles: {
+      text: {
+        color: '#000000'
+      }
+    }
   }
 
-  renderAnswer(key) {
+  renderDescription(key) {
     let description = this.props.numbered ? '' + key + '.' : '';
-    return <JournalTextInput {...this.props}
-      key={key}
-      description={description}
-    ></JournalTextInput>
+    if (description) {
+      return (
+        <Text style={[styles.description, this.props.styles.text]}>
+          {description}
+        </Text>
+      );
+    }
+  }
+
+  renderAnswer() {
+    return <JournalTextInput {...this.props} ></JournalTextInput>
   }
 
   render() {
     let answers = [];
     for(let i = 1; i<=this.props.count; i++) {
-      answers.push(this.renderAnswer(i));
+      answers.push(<View style={styles.container} key={i}>
+        {this.renderDescription(i)}
+        {this.renderAnswer()}
+      </View>)
     }
     return (<View>{answers}</View>)
   }
 }
+
+const styles = StyleSheet.create({
+  description: {
+    width: 15,
+    marginRight: margin
+  },
+  container: {
+    flexDirection: 'row',
+    margin: margin,
+    marginTop: 20
+  }
+});
