44c2a1c186095cf073d14de29037f951629812f8
athomschke <astrid.thomschke@gmail.com>
2016-06-24 12:28:29 +0200

    Remove configureStore file as it is not used anymore to set any state #5

--- app/Application.js
+++ app/Application.js
@@ -1,17 +1,18 @@
 // ./node_modules/react-native/packager/packager.sh start --resetCache
 
 import React, { Component } from 'react';
-import configureStore from './configureStore';
 import { Provider } from 'react-redux';
-import { AsyncStorage } from 'react-native';
+import { createStore } from 'redux';
+import { View } from 'react-native';
 import Journal from './containers/Journal';
+import journal from './reducers/journal';
 import loadData from './loadData';
 
 export default class Application extends Component {
 
   render() {
 
-    let store = configureStore();
+    let store = createStore(journal);
 
     loadData(store);
 
--- app/configureStore.js
+++
@@ -1,7 +0,0 @@
-// ./node_modules/react-native/packager/packager.sh start --resetCache
-import { createStore } from 'redux';
-import journal from './reducers/journal';
-
-export default function () {
-  return createStore(journal);
-};
